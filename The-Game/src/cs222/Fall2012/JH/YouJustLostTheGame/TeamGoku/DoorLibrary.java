3168e6860c569e1f314cec45c3ca348da2bf426e
Jeffrey McDaniel <jsmcdaniel@bsu.edu>
2015-07-14 17:53:55 -0400

    first commit

--- The-Game/src/cs222/Fall2012/JH/YouJustLostTheGame/TeamGoku/DoorLibrary.java
+++
@@ -1,9 +0,0 @@
-package cs222.Fall2012.JH.YouJustLostTheGame.TeamGoku;
-
-import java.util.Hashtable;
-
-public class DoorLibrary {
-	
-	public static Hashtable<String,Door> doorLibrary = new Hashtable<String,Door>();
-
-}
